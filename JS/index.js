f6809ec8de8f44742f91e1dea06112a609fd8551
IsaacMurage-softwaredeveloper <isaac.moringa@student.moringaschool.com>
2021-07-18 11:51:53 +0300

    introduce first alert message

--- JS/index.js
+++ JS/index.js
@@ -6,6 +6,7 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // conversion
+// function
 function akanName() {
     var dd = parseInt(document.getElementById("DD").value);
     var mm = parseInt(document.getElementById("MM").value);
@@ -15,3 +16,7 @@ function akanName() {
 
 	var date = new Date(yy + "-" + mm + "-" + dd);
     var dayBorn = date.getDay();
+
+	if (dd < 1 || dd > 31) {
+		alert("Enter a valid day!");
+	  }
